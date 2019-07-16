4a12228b78dc5f8fc2d3fe61ec61eb55fd396c9d
irfan0113 <irfanera1331@gmail.com>
2019-07-16 10:48:05 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Stack(
+        children: <Widget>[
+          ClipPath(
+            child: Container(
+              color: Colors.black.withOpacity(0.8)
+            ),
+            clipper: getClipper(),
+          ),
+          Positioned(
+            width : 350.0,
+            top: MediaQuery.of(context).size.height/5,
+            child:Column(
+              children: <Widget>[
+                Container(
+                  width: 150.0,
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    image: DecorationImage(
+                      image: NetworkImage('https://miro.medium.com/max/400/1*gH1iKXJH8T12LIqhboZWEA.png'),
+                      fit: BoxFit.cover
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(75.0)
+                    )
+                  ),
+                )
+              ],
+              
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class getClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+
+    path.lineTo(0.0, size.height/1.9);
+    path.lineTo(size.width + 125, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper){
+    return true;
+  }
+}
